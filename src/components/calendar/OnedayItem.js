aea3b127d66745c4a7c3fd14d448cc26618856de
24seconds <24crazyoung@gmail.com>
2018-08-01 19:29:09 +0000

    Implement today list color

--- src/components/calendar/OnedayItem.js
+++ src/components/calendar/OnedayItem.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import './OnedayItem.css';
+import moment from 'moment';
+
 
 import DateItem from './DateItem'
 import EventList  from './EventList'
@@ -10,12 +12,13 @@ class OnedayItem extends Component {
 
   render (){
     const {date, events, oneItemId, handleMouseEnterToItem, handleShow} = this.props;
+    const isToday = moment().format("YYYY-MM-DD") === date.format("YYYY-MM-DD");
     // console.log('OnedayItem');
     // console.log(date.format("YYYY-MM-DD"));
 
 
     return(
-      <div className = "OnedayItem-template">
+      <div className = {`OnedayItem-template ${isToday && 'OnedayItem-isToday'}`}>
         <DateItem date={date} key = {oneItemId}/>
         <EventList date={date} events={events} key = {oneItemId} handleMouseEnterToItem = {handleMouseEnterToItem} handleShow = {handleShow}/>
       </div>
@@ -27,3 +30,7 @@ class OnedayItem extends Component {
 
 
 export default OnedayItem;
+
+
+// {`EventItem-template ${istoday && 'isToday'}` +
+//  (diff & !istoday ? ' isPastday' : (isfutureday ? ' isFutureday' : ' nono'))}
